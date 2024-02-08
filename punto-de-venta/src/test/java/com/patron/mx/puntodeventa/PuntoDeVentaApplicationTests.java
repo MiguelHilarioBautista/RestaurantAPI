12b777fb02ec9ef1984c7c547d5492deca8eb7a4
Miguel Hilario Bautista <miguel.hilario.0219@gmail.com>
2024-02-08 15:18:08 -0600

    feat:Init

---
+++ punto-de-venta/src/test/java/com/patron/mx/puntodeventa/PuntoDeVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patron.mx.puntodeventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoDeVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
